48e3eff17f1958521dcfdb9c24d263efb612556c
Shaeen <shaeen@rapidtrade.biz>
2018-06-26 20:10:00 +0200

    removed angularjs feature

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('netflix', ["ngRoute"]);
-
-app.config(function($routeProvider) {
-  $routeProvider
-    .when("/cancelanytime", {templateUrl: "js/core/views/cancelanytime.html"})
-    .when("/watchanywhere", {templateUrl: "js/core/views/watchanywhere.html"})
-    .when("/pickprice", {templateUrl: "js/core/views/pickprice.html"})
-    .otherwise({redirectTo: "/"})
-});
